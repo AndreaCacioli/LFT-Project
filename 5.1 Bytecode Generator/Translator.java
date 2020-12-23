343ba49e8cfda540fbe62c49b8f9b7eb2ebcdfe6
Andrea Cacioli <andrea.cacioli.1c@gmail.com>
2020-12-23 16:14:24 +0100

    Setup Translator

--- 5.1 Bytecode Generator/Translator.java
+++ 5.1 Bytecode Generator/Translator.java
@@ -8,7 +8,7 @@ public class Translator
 
     SymbolTable st = new SymbolTable();
     CodeGenerator code = new CodeGenerator();
-    int count=0;
+    int count=0; //Tiene il numero di variabili nel programma
 
     public Translator(Lexer l, BufferedReader br)
     {
@@ -40,11 +40,20 @@ public class Translator
 
     public void prog()//S
     {
-	     // ... completare ...
+      switch(look.tag)
+      {
+        case '=':
+        case Tag.READ:
+        case Tag.WHILE:
+        case Tag.PRINT:
+        case Tag.CASE:
+        case '{':
         int lnext_prog = code.newLabel();
         statlist(lnext_prog);
         code.emitLabel(lnext_prog);
         match(Tag.EOF);
+        break;
+      }
         try
         {
         	code.toJasmin();
@@ -53,10 +62,12 @@ public class Translator
         {
         	System.out.println("IO error\n");
         };
-	       // ... completare ...
+	      default:
+        System.err.println("BIG OOF TIME");
+        break;
     }
 
-        private void statlist(int )//A
+        private void statlist(int lnext_prog)//A
         {
           switch(look.tag)
           {
@@ -92,11 +103,41 @@ public class Translator
 
         }
 
-    public void stat( /* completare */ )
+    public void stat( /* completare */ ) //C
     {
         switch(look.tag)
         {
-	// ... completare ...
+            case '=':
+            match('=');
+            match(257);
+            expr();
+            break;
+            case Tag.WHILE:
+            match(Tag.WHILE);
+            match('(');
+            bexpr();
+            match(')');
+            stat();
+            break;
+            case Tag.PRINT:
+            match(Tag.PRINT);
+            match('(');
+            exprlist();
+            match(')');
+            break;
+            case Tag.CASE:
+            match(Tag.CASE);
+            whenlist();
+            match(Tag.ELSE);
+            stat();
+            break;
+            case '{':
+            match('{');
+            statlist();
+            match('}');
+            break;
+
+
             case Tag.READ:
                 match(Tag.READ);
                 match('(');
@@ -116,25 +157,148 @@ public class Translator
                 else
                     error("Error in grammar (stat) after read( with " + look);
                 break;
-	// ... completare ...
+            default:
+            error("C");
+            break;
         }
+     }
+
+     private void whenlist()//D
+     {
+       if(look.tag == Tag.WHEN)
+       {
+         whenitem();
+         whenlistp();
+       }
+       else
+       {
+         error("D");
+       }
+     }
+
+     private void whenlistp()//E
+     {
+       if(look.tag == Tag.WHEN)
+       {
+         whenitem();
+         whenlistp();
+       }
+       else if(look.tag == Tag.ELSE)
+       {
+         //eps
+       }
+       else
+       {
+         error("E");
+       }
+     }
+     private void whenitem()//F
+     {
+       if(look.tag == Tag.WHEN)
+       {
+         match(Tag.WHEN);
+         match('(');
+         bexpr();
+         match(')');
+         match(Tag.DO);
+         stat();
+
+       }
+       else
+       {
+         error("F");
+       }
+     }
+     private void bexpr()//G
+     {
+       if(look.tag == Tag.RELOP)
+       {
+         match(Tag.RELOP);
+         expr();
+         expr();
+       }
+       else
+       {
+         error("G");
+       }
      }
 
     private void expr( /* completare */ )//H
     {
         switch(look.tag)
         {
-	// ... completare ...
+          case '+':
+          match('+');
+          match('(');
+          exprlist();
+          match(')');
+          break;
+          case '*':
+          match('*');
+          match('(');
+          exprlist();
+          match(')');
+          break;
+
             case '-':
                 match('-');
                 expr();
                 expr();
                 code.emit(OpCode.isub);
                 break;
-	// ... completare ...
+                case '/':
+                match('/');
+                expr();
+                expr();
+                break;
+                case 257:
+                match(257);
+                break;
+                case 256:
+                match(NumberTok.tag);
+                break;
+                default:
+                error("H");
+                break;
         }
     }
-
+    private void exprlist()//I
+    {
+      switch(look.tag)
+      {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case 257:
+        case NumberTok.tag:
+        expr();
+        exprlistp();
+        break;
+        default:
+        error("I");
+        break;
+      }
+    }
+    private void exprlistp()//J
+    {
+      switch(look.tag)
+      {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case 257:
+        case NumberTok.tag:
+        expr();
+        exprlistp();
+        break;
+        case ')':
+        break;
+        default:
+        error("J");
+      }
+    }
     public static void main(String[] args)
     {
         Lexer lex = new Lexer();
